70dcd1344f56159a06e787e9c3978325a22eeb46
Alessandro De Marco <lookatde.lad@gmail.com>
2021-02-03 10:38:46 +0100

    End of milestone 4

--- js/script.js
+++ js/script.js
@@ -7,25 +7,25 @@ var app = new Vue({
         propic: 'img/avatar_2.jpg',
         chat: [
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:20',
             type: 'received',
             text: 'Hey, ciao Michele!'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:22',
             type: 'sent',
             text: 'Ciao!'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:23',
             type: 'received',
             text: 'Ti sei ricordato di comprare il regalo per la festa di Manuela?'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:23',
             type: 'sent',
             text: 'No, cavolo! Adesso vado subito a prenderlo!'
@@ -37,25 +37,25 @@ var app = new Vue({
         propic: 'img/avatar_3.jpg',
         chat: [
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:20',
             type: 'received',
             text: 'Hey, ciao Fabio!'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:22',
             type: 'sent',
             text: 'Ciao! Come stai?'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:23',
             type: 'received',
             text: 'Bene grazie! Tu? Sabato esco con Michela e Anna, vuoi venire?'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:23',
             type: 'sent',
             text: 'Sto bene, grazie! si va bene, che bello uscire tutti insieme!'
@@ -67,25 +67,25 @@ var app = new Vue({
         propic: 'img/avatar_4.jpg',
         chat: [
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:20',
             type: 'received',
             text: 'Hey, ciao Samuele!'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:22',
             type: 'sent',
             text: 'Ciao! Come stai?'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:23',
             type: 'received',
             text: 'Non ci lamentiamo, tu?'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:23',
             type: 'sent',
             text: 'Anche io tutto bene, grazie!'
@@ -97,25 +97,25 @@ var app = new Vue({
         propic: 'img/avatar_5.jpg',
         chat: [
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:20',
             type: 'received',
             text: 'Hey, ciao Mario!'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:22',
             type: 'sent',
             text: 'Ciao! Come stai?'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:23',
             type: 'received',
             text: 'Bene dai, grazie! Tu?'
           },
           {
-            day: '2 feb 2020',
+            day: 'Tue 2 Feb',
             hour: '10:23',
             type: 'sent',
             text: 'Anche io tutto bene, sto frequentando un corso bellissimo!'
@@ -132,24 +132,37 @@ var app = new Vue({
       text: ''
     },
     replyComplete: {
-      day: '2 feb 2020',
-      hour: '10:26',
+      day: dayjs().format('ddd D MMM'),
+      hour: dayjs().format('HH:mm'),
       type: 'received',
       text: 'Okay'
-    }
+    },
+    filtered: [],
+    newFilter: ''
   },
   created(){
-    this.selected = this.contacts[0];
+    this.filtered = this.contacts;
+    this.selected = this.filtered[0];
   },
   methods: {
     messageSent() {
-      this.newMessComplete.day = '2 feb 2020';
-      this.newMessComplete.hour = '10:25';
+      this.newMessComplete.day = dayjs().format('ddd D MMM');
+      this.newMessComplete.hour = dayjs().format('HH:mm');
       this.newMessComplete.type = 'sent';
       this.newMessComplete.text = this.newMess;
       this.newMess = '';
       this.selected.chat.splice(this.selected.chat.length, 0, this.newMessComplete);
-      setTimeout(() => this.selected.chat.splice(this.selected.chat.length, 0, this.replyComplete), 1000)
+      setTimeout(() => this.selected.chat.splice(this.selected.chat.length, 0, this.replyComplete), 1000);
+    },
+    filterChat() {
+      this.filtered = [];
+      this.newFilter = this.newFilter.charAt(0).toUpperCase() + this.newFilter.slice(1);
+      for (var i = 0; i < this.contacts.length; i++) {
+        if (this.contacts[i].name.includes(this.newFilter)) {
+          this.filtered.push(this.contacts[i]);
+          console.log(this.newFilter);
+        }
+      }
     }
   }
 });
